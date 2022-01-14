3614bed174cd50e92ce0147d71411b1746c02c0d
Patrick Hirata (Marlo) <patrick.hirata@marlo.com.au>
2022-01-14 15:00:16 -0300

    still adding important files

---
+++ src/test/java/com/example/crossjoinchallenge/CrossJoinChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crossjoinchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossJoinChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
